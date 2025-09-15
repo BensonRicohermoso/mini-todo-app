703c7ba77a49ecf7e16ab6cf96896fea7a708d31
zxcy <bensonricohermoso52@gmail.com>
2025-09-15 17:20:37 +0800

    Save local changes before merge

--- d2-todo_app/todo.py
+++ d2-todo_app/todo.py
@@ -1,7 +1,13 @@
 print ("Welcome to to-do app")
 action = input("What do you want to do? (add, edit, show, exit): ")
 
+
+
 while action != "exit":
     if action == "add":
-        
+        item = input("What do you want to add? ")
+    elif action == "edit":
+        delete = input("What do you want to delete? ")
+    elif action == "show":
+        print(display)
              
