1aedd4309093c21296fc1019db2131e8983ee164
zxcy <bensonricohermoso52@gmail.com>
2025-09-15 21:03:25 +0800

    Update todo.py

--- d2-todo_app/todo.py
+++ d2-todo_app/todo.py
@@ -5,9 +5,39 @@ action = input("What do you want to do? (add, edit, show, exit): ")
 
 while action != "exit":
     if action == "add":
-        item = input("What do you want to add? ")
-    elif action == "edit":
-        delete = input("What do you want to delete? ")
+        todo = input("Enter a to-do item: ")
+        with open("todos.txt", "a") as file:
+            file.write(todo + "\n")
+        print(f'Added to-do: "{todo}"')
+
     elif action == "show":
-        print(display)
-             
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        print("Your to-do list:")
+        for index, item in enumerate(todos, start=1):
+            print(f"{index}. {item.strip()}")
+
+    elif action == "edit":
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        print("Your to-do list:")
+        for index, item in enumerate(todos, start=1):
+            print(f"{index}. {item.strip()}")
+        try:
+            item_number = int(input("Enter the number of the item you want to edit: "))
+            if 1 <= item_number <= len(todos):
+                new_todo = input("Enter the new to-do item: ")
+                todos[item_number - 1] = new_todo + "\n"
+                with open("todos.txt", "w") as file:
+                    file.writelines(todos)
+                print(f'Updated item {item_number} to "{new_todo}"')
+            else:
+                print("Invalid item number.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+    else:
+        print("Invalid action. Please choose from (add, edit, show, exit).")
+
+    action = input("What do you want to do? (add, edit, show, exit): ")
+    
